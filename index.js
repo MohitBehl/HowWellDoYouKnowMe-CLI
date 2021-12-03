e32e2915a29782eece5ed6a579a011d5a21ddfa9
MohitBehl <mohitbehlofficial@gmail.com>
2021-12-03 15:14:26 +0530

    FEAT : implemented chalk.

--- index.js
+++ index.js
@@ -19,12 +19,14 @@ var questions = [
     }
 ]
 
-var readLineSync = require("readline-sync");
+// var readLineSync = require("readline-sync"); .... was not able to use it because of type:module in the package.json
+import readLineSync from "readline-sync";
+import chalk from 'chalk';
 
 // MAIN LOGIC
 var userName = readLineSync.question("Whats your name  ? ");
 var score = 0;
-
+var countCorrect = 0 , countIncorrect = 0;
 console.clear();
 greetings(userName);
 
@@ -32,35 +34,38 @@ var res = shallWeStartTheQuiz();
 
 if(res){
     console.clear();
-    console.log("GAME TIME !!!");
+    console.log(chalk.cyanBright(chalk.bold("GAME TIME !!!")));
     console.log("-------------------------------------------------");
 
     for(var i = 0 ; i < questions.length ; i++){
-        console.log("Current Score : "+score);
+        console.log(chalk.yellowBright("Current Score : "+score));
         console.log();
         var userRes = readLineSync.question("Ques."+(i+1)+" "+questions[i].ques);
         if(userRes.toUpperCase() == questions[i].answer){
             score = score + 1;
-            console.log("answer is Correct");
+            console.log(chalk.greenBright("answer is Correct"));
+            countCorrect = countCorrect + 1;
         }else{
             score = score - 1;
-            console.log("answer is wrong");
+            console.log(chalk.redBright("answer is wrong"));
+            countIncorrect = countIncorrect + 1;
         }
 
         console.log("-------------------------------------------------");
         console.log("-------------------------------------------------");
     }
 }
+console.clear();
 goodBye();
 
 // HELPER FUNCTIONS
 // greetings
 function greetings(name){
     console.log("-------------------------------------------------");
-    console.log("Hello "+name);
+    console.log(chalk.cyan(chalk.bold("Hello "+name)));
     console.log("Welcome to my quiz !");
     console.log("------------------------------------------------ ");
-    console.log("NOTE : Correct Rsponses will get you scores.")
+    console.log(chalk.red("NOTE : Correct Rsponses will get you scores."));
 }
 
 function shallWeStartTheQuiz(){
@@ -75,12 +80,15 @@ function shallWeStartTheQuiz(){
 
 function goodBye(){
     printScore();
+    console.log();
+    console.log();
     console.log("Thanks for your responses !!!");
     console.log("Good Bye !!! , Have a great day ahead ...");
 }
 
 function printScore(){
-    console.log("Your final Score : "+score);
+    console.log("Correct : "+chalk.greenBright(countCorrect)+"\nIncorrect : "+chalk.redBright(countIncorrect));
+    console.log("Total Questions : 5 \n"+chalk.blackBright(chalk.bold(chalk.underline("Score : "+score))));
 }
 
 
